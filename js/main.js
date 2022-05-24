76e5da7a5d575db1b999d69916849bf932bc48d1
MohamedTarek1993 <www.mtarek789@gmail.com>
2022-05-24 18:14:42 +0300

    24/5

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //loader
- let loader = document.getElementById('preloader');
- window.addEventListener('load', function () {  loader.style.display = 'none';
- });
+//  let loader = document.getElementById('preloader');
+//  window.addEventListener('load', function () {  loader.style.display = 'none';
+//  });
 //loader
 
 //  scroll-up 
---
+++ ui-part/team.php
@@ -0,0 +1,95 @@
+<?php 
+
+
+?>
+
+<!---------------------- start team-------------------------------------------->
+<div class="team_section" data-aos="fade-up" data-aos-duration="3000">
+        <div class="container">
+            <div class="main-title">
+                <h2>فريق العمل</h2>
+                <p>ﻫﻲ ﺷﺮﻛﺔ ﻣﻬﻨﻴﺔ ﻣﺮﺧﺼﺔ ﺗﻀﻢ ﻋﺪد ﻣﻦ اﻟﻤﺤﺎﻣﻴﻦ واﻟﻤﺴﺘﺸﺎرﻳﻦ اﻟﻤﺘﺨﺼﺼﻴﻦ ﻓﻲ اﻟﻤﺠﺎل اﻟﻘﺎﻧﻮﻧﻲ واﻟﺸﺮﻋﻲ ﻣﻦ ذوي
+                    اﻟﻤﺆﻫﻼت اﻟﻌﺎﻟﻴﺔ و اﻟﺨﺒﺮة اﻟﻄﻮﻳﻠﺔ ﺑﺎﻷﻧﻈﻤﺔ اﻟﺴﻌﻮدﻳﺔ واﻹﻗﻠﻴﻤﻴﺔ اﻟﺘﻲ أﻛﺘﺴﺒﻨﺎﻫﺎ ﻋﻠﻰ ﻣﺪار ﺛﻼﺛﺔ ﻋﺸﺮَ ﺳﻨﺔ
+                </p>
+            </div>
+            <div class="swiper mySwiper-1">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="container_card">
+                            <div class="box-1">
+                                <div class="caption-1">
+                                    <a target="_blank" href="#"><i class="bi bi-facebook"></i></a>
+                                    <a target="_blank" href="#"><i class="bi bi-instagram"></i></a>
+                                    <a target="_blank" href="#"><i class="bi bi-linkedin"></i></a>
+                                    <a target="_blank" href="#"><i class="bi bi-twitter"></i></a>
+                                </div>
+                                <img src="image/team1.jpg" alt="1">
+
+                            </div>
+                            <div class="content">
+                                <h6 class="text">محمد عبد الرحمن</h6>
+                                <p class="parg">الشريك المؤسس , الرئيس التنفيذي</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="container_card">
+                            <div class="box-1">
+                                <div class="caption-1">
+                                    <a target="_blank" href="#"><i class="bi bi-facebook"></i></a>
+                                    <a target="_blank" href="#"><i class="bi bi-instagram"></i></a>
+                                    <a target="_blank" href="#"><i class="bi bi-linkedin"></i></a>
+                                    <a target="_blank" href="#"><i class="bi bi-twitter"></i></a>
+                                </div>
+                                <img src="image/team2.jpg" alt="1">
+
+                            </div>
+                            <div class="content">
+                                <h6 class="text">محمد عبد الرحمن</h6>
+                                <p class="parg">الشريك المؤسس , الرئيس التنفيذي</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="container_card">
+                            <div class="box-1">
+                                <div class="caption-1">
+                                    <a target="_blank" href="#"><i class="bi bi-facebook"></i></a>
+                                    <a target="_blank" href="#"><i class="bi bi-instagram"></i></a>
+                                    <a target="_blank" href="#"><i class="bi bi-linkedin"></i></a>
+                                    <a target="_blank" href="#"><i class="bi bi-twitter"></i></a>
+                                </div>
+                                <img src="image/team3.jpg" alt="1">
+
+                            </div>
+                            <div class="content">
+                                <h6 class="text">محمد عبد الرحمن</h6>
+                                <p class="parg">الشريك المؤسس , الرئيس التنفيذي</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="container_card">
+                            <div class="box-1">
+                                <div class="caption-1">
+                                    <a target="_blank" href="#"><i class="bi bi-facebook"></i></a>
+                                    <a target="_blank" href="#"><i class="bi bi-instagram"></i></a>
+                                    <a target="_blank" href="#"><i class="bi bi-linkedin"></i></a>
+                                    <a target="_blank" href="#"><i class="bi bi-twitter"></i></a>
+                                </div>
+                                <img src="image/team4.jpg" alt="1">
+
+                            </div>
+                            <div class="content">
+                                <h6 class="text">محمد عبد الرحمن</h6>
+                                <p class="parg">الشريك المؤسس , الرئيس التنفيذي</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-button-next"><i class="bi bi-arrow-left"></i></div>
+                <div class="swiper-button-prev"><i class="bi bi-arrow-right"></i></div>
+            </div>
+        </div>
+    </div>
+    <!---------------------- finish team-------------------------------------------->
